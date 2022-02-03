52eccd96be94a2aff89de28d61c81f19ea8a18b9
98lxh <582658211@qq.com>
2022-02-03 11:23:50 +0800

    feat:播放菜单样式

--- src/pages/player/cpns/playerMenu/index.tsx
+++ src/pages/player/cpns/playerMenu/index.tsx
@@ -1,21 +1,46 @@
 import { Col, Drawer, Row } from "antd";
 import React, { memo } from "react";
-
+import './index.less'
+import {AppstoreAddOutlined, DeleteOutlined, ShareAltOutlined, UnorderedListOutlined,DownloadOutlined} from "@ant-design/icons";
 interface IPlayMenuProps {
   isShow: boolean;
   onClose: () => void;
 }
 const playerMenu: React.FC<IPlayMenuProps> = memo((props) => {
   return (
-    <Drawer
-      title="播放菜单"
+    <Drawer className="player-menu-wrapper" title="播放菜单"
       visible={props.isShow}
       onClose={props.onClose}
-      placement="top"
+      placement="bottom"
     >
       <Row gutter={24}>
-        <Col className="muisc-list"></Col>
-        <Col className="muisc-info"></Col>
+        <Col sm={24} xs={24} md={10} lg={10} xl={10} className="music-info">
+          <div className="music-info-title">海底</div>
+        </Col>
+        <Col sm={24} xs={24} md={14} lg={14} xl={14}  className="music-list">
+          <div className="music-menu-title">
+              <div className="music-menu-icon">
+                <UnorderedListOutlined />
+                <p>播放列表(2)</p>
+              </div>
+              <div className="music-menu-clear">
+                <DeleteOutlined />
+                <p>清除</p>
+              </div>
+          </div>
+          <ul className="music-menu-list">
+            <li className="music-menu-item">
+              <p className="music-name">海底</p>
+              <div className="music-handle">
+                <AppstoreAddOutlined />
+                <DeleteOutlined />
+                <DownloadOutlined />
+              </div>
+              <p className="music-time">02:31</p>
+              <ShareAltOutlined />
+            </li>
+          </ul>
+        </Col>
       </Row>
     </Drawer>
   );
